ec35a290604b2c1786a1c804643324d1e89eaee6
derenyilmaz <3rendeniz@gmail.com>
2024-01-13 13:40:54 +0300

    feat: create photos app

--- mysite/urls.py
+++ mysite/urls.py
@@ -21,4 +21,5 @@ from django.urls import include, path
 urlpatterns = [
     path("polls/", include("polls.urls")),
     path("admin/", admin.site.urls),
+    path("photos/", include("photos.urls")),
 ]
